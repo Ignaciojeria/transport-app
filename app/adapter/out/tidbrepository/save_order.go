29f50f89dee70fad615c7ed63254b876af4c3a4d
ignaciojeria <ignaciovl.j@gmail.com>
2025-01-08 21:51:34 -0300

    save order improvements

--- app/adapter/out/tidbrepository/save_order.go
+++ app/adapter/out/tidbrepository/save_order.go
@@ -45,39 +45,39 @@ func NewSaveOrder(
 		var result QueryResult
 
 		err := conn.Raw(`
-	WITH 
-	  org_country AS (
+		WITH 
+		  org_country AS (
+			SELECT 
+			  organization_countries.id AS organization_country_id
+			FROM 
+			  organization_countries
+			INNER JOIN api_keys ON api_keys.organization_id = organization_countries.organization_id
+			WHERE 
+			  organization_countries.country = ? AND api_keys.key = ?
+		  )
 		SELECT 
-		  organization_countries.id AS organization_country_id
+		  org_country.organization_country_id AS organization_country_id,
+		  c.id AS commerce_id,
+		  con.id AS consumer_id,
+		  ot.id AS order_type_id,
+		  o_ct.id AS origin_contact_id,
+		  d_ct.id AS destination_contact_id,
+		  o_ai.id AS origin_address_id,
+		  d_ai.id AS destination_address_id,
+		  o_ni.id AS origin_node_info_id,
+		  d_ni.id AS destination_node_info_id
 		FROM 
-		  organization_countries
-		INNER JOIN api_keys ON api_keys.organization_id = organization_countries.organization_id
-		WHERE 
-		  organization_countries.country = ? AND api_keys.key = ?
-	  )
-	SELECT 
-	  org_country.organization_country_id AS organization_country_id,
-	  c.id AS commerce_id,
-	  con.id AS consumer_id,
-	  ot.id AS order_type_id,
-	  o_ct.id AS origin_contact_id,
-	  d_ct.id AS destination_contact_id,
-	  o_ai.id AS origin_address_id,
-	  d_ai.id AS destination_address_id,
-	  o_ni.id AS origin_node_info_id,
-	  d_ni.id AS destination_node_info_id
-	FROM 
-	  org_country
-	LEFT JOIN commerces c ON c.organization_country_id = org_country.organization_country_id AND c.name = ?
-	LEFT JOIN consumers con ON con.organization_country_id = org_country.organization_country_id AND con.name = ?
-	LEFT JOIN order_types ot ON ot.organization_country_id = org_country.organization_country_id AND ot.type = ?
-	LEFT JOIN contacts o_ct ON o_ct.organization_country_id = org_country.organization_country_id AND o_ct.full_name = ? AND o_ct.email = ? AND o_ct.phone = ?
-	LEFT JOIN contacts d_ct ON d_ct.organization_country_id = org_country.organization_country_id AND d_ct.full_name = ? AND d_ct.email = ? AND d_ct.phone = ?
-	LEFT JOIN address_infos o_ai ON o_ai.organization_country_id = org_country.organization_country_id AND o_ai.raw_address = ?
-	LEFT JOIN address_infos d_ai ON d_ai.organization_country_id = org_country.organization_country_id AND d_ai.raw_address = ?
-	LEFT JOIN node_infos o_ni ON o_ni.organization_country_id = org_country.organization_country_id AND o_ni.reference_id = ?
-	LEFT JOIN node_infos d_ni ON d_ni.organization_country_id = org_country.organization_country_id AND d_ni.reference_id = ?;
-			`,
+		  org_country
+		LEFT JOIN commerces c ON c.organization_country_id = org_country.organization_country_id AND c.name = ?
+		LEFT JOIN consumers con ON con.organization_country_id = org_country.organization_country_id AND con.name = ?
+		LEFT JOIN order_types ot ON ot.organization_country_id = org_country.organization_country_id AND ot.type = ?
+		LEFT JOIN contacts o_ct ON o_ct.organization_country_id = org_country.organization_country_id AND o_ct.full_name = ? AND o_ct.email = ? AND o_ct.phone = ?
+		LEFT JOIN contacts d_ct ON d_ct.organization_country_id = org_country.organization_country_id AND d_ct.full_name = ? AND d_ct.email = ? AND d_ct.phone = ?
+		LEFT JOIN address_infos o_ai ON o_ai.organization_country_id = org_country.organization_country_id AND o_ai.raw_address = ?
+		LEFT JOIN address_infos d_ai ON d_ai.organization_country_id = org_country.organization_country_id AND d_ai.raw_address = ?
+		LEFT JOIN node_infos o_ni ON o_ni.organization_country_id = org_country.organization_country_id AND o_ni.reference_id = ?
+		LEFT JOIN node_infos d_ni ON d_ni.organization_country_id = org_country.organization_country_id AND d_ni.reference_id = ?;
+				`,
 			to.Organization.Country.Alpha2(),
 			to.Organization.Key,
 			to.BusinessIdentifiers.Commerce,
@@ -102,45 +102,50 @@ func NewSaveOrder(
 		return domain.Order{}, conn.Transaction(func(tx *gorm.DB) error {
 			orderTable := mapper.MapOrderToTable(to)
 
-			saveIfNotExists := func(id int64, entity interface{}) error {
-				if id == 0 {
-					return tx.Save(entity).Error
+			if result.CommerceID == 0 {
+				orderTable.Commerce.OrganizationCountryID = result.OrganizationCountryID
+				if err := tx.Save(&orderTable.Commerce).Error; err != nil {
+					return err
 				}
-				return nil
 			}
 
-			if err := saveIfNotExists(result.CommerceID, &orderTable.Commerce); err != nil {
-				return err
+			if result.ConsumerID == 0 {
+				orderTable.Consumer.OrganizationCountryID = result.OrganizationCountryID
+				if err := tx.Save(&orderTable.Consumer).Error; err != nil {
+					return err
+				}
 			}
 
-			if err := saveIfNotExists(result.ConsumerID, &orderTable.Consumer); err != nil {
-				return err
-			}
-
-			if err := saveIfNotExists(result.OriginContactID, &orderTable.OriginContact); err != nil {
-				return err
+			if result.OriginContactID == 0 {
+				orderTable.OriginContact.OrganizationCountryID = result.OrganizationCountryID
+				if err := tx.Save(&orderTable.OriginContact).Error; err != nil {
+					return err
+				}
 			}
 
 			if result.DestinationContactID == 0 {
 				if to.IsOriginAndDestinationContactEqual() {
 					orderTable.DestinationContact.ID = orderTable.OriginContact.ID
 				} else {
-					orderTable.DestinationContact.ID = 0
+					orderTable.DestinationContact.OrganizationCountryID = result.OrganizationCountryID
 					if err := tx.Save(&orderTable.DestinationContact).Error; err != nil {
 						return err
 					}
 				}
 			}
 
-			if err := saveIfNotExists(result.OriginAddressID, &orderTable.OriginAddressInfo); err != nil {
-				return err
+			if result.OriginAddressID == 0 {
+				orderTable.OriginAddressInfo.OrganizationCountryID = result.OrganizationCountryID
+				if err := tx.Save(&orderTable.OriginAddressInfo).Error; err != nil {
+					return err
+				}
 			}
 
 			if result.DestinationAddressID == 0 {
 				if to.IsOriginAndDestinationAddressEqual() {
 					orderTable.DestinationAddressInfo.ID = orderTable.OriginAddressInfo.ID
 				} else {
-					orderTable.DestinationAddressInfo.ID = 0
+					orderTable.DestinationAddressInfo.OrganizationCountryID = result.OrganizationCountryID
 					if err := tx.Save(&orderTable.DestinationAddressInfo).Error; err != nil {
 						return err
 					}
@@ -159,7 +164,6 @@ func NewSaveOrder(
 				if to.IsOriginAndDestinationNodeEqual() {
 					orderTable.DestinationNodeInfo.ID = orderTable.OriginNodeInfo.ID
 				} else {
-					orderTable.DestinationNodeInfo.ID = 0
 					orderTable.DestinationNodeInfo.OrganizationCountryID = result.OrganizationCountryID
 					orderTable.DestinationNodeInfo.AddressID = orderTable.DestinationAddressInfo.ID
 					if err := tx.Save(&orderTable.DestinationNodeInfo).Error; err != nil {
