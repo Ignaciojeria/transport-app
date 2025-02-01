618d77b4dc18d3a19de184fd2829bb89a92c0328
ignaciojeria <ignaciovl.j@gmail.com>
2025-01-31 21:06:49 -0300

    unimplemented endpoints unregistered

--- app/adapter/in/fuegoapi/create_account_dispatcher.go
+++ app/adapter/in/fuegoapi/create_account_dispatcher.go
@@ -1,17 +1,10 @@
 package fuegoapi
 
 import (
-	"net/http"
-	"transport-app/app/adapter/in/fuegoapi/request"
-	"transport-app/app/adapter/in/fuegoapi/response"
 	"transport-app/app/shared/infrastructure/httpserver"
 	"transport-app/app/usecase"
 
 	ioc "github.com/Ignaciojeria/einar-ioc/v2"
-	"github.com/biter777/countries"
-	"github.com/go-fuego/fuego"
-	"github.com/go-fuego/fuego/option"
-	"github.com/go-fuego/fuego/param"
 )
 
 func init() {
@@ -23,29 +16,30 @@ func init() {
 func createAccount(
 	s httpserver.Server,
 	createAccount usecase.CreateAccount) {
-	fuego.Post(s.Manager, "/account/dispatcher",
-		func(c fuego.ContextWithBody[request.CreateDispatcherRequest]) (response.CreateAccountResponse, error) {
-			requestBody, err := c.Body()
-			if err != nil {
-				return response.CreateAccountResponse{}, err
-			}
-			acc := requestBody.Map()
-			acc.Organization.Key = c.Header("organization-key")
-			acc.Organization.Country = countries.ByName(c.Header("country"))
-			_, err = createAccount(c.Context(), acc)
-			if err != nil {
-				return response.CreateAccountResponse{}, fuego.HTTPError{
-					Title:  "error creating account",
-					Detail: err.Error(),
-					Status: http.StatusInternalServerError,
+	/*
+		fuego.Post(s.Manager, "/account/dispatcher",
+			func(c fuego.ContextWithBody[request.CreateDispatcherRequest]) (response.CreateAccountResponse, error) {
+				requestBody, err := c.Body()
+				if err != nil {
+					return response.CreateAccountResponse{}, err
 				}
-			}
-			return response.CreateAccountResponse{
-				Message: "account created",
-			}, nil
-		}, option.Summary("createAccountDispatcher"),
-		option.Header("organization-key", "api organization key", param.Required()),
-		option.Header("country", "api country", param.Required()),
-		option.Tags(tagAccounts),
-	)
+				acc := requestBody.Map()
+				acc.Organization.Key = c.Header("organization-key")
+				acc.Organization.Country = countries.ByName(c.Header("country"))
+				_, err = createAccount(c.Context(), acc)
+				if err != nil {
+					return response.CreateAccountResponse{}, fuego.HTTPError{
+						Title:  "error creating account",
+						Detail: err.Error(),
+						Status: http.StatusInternalServerError,
+					}
+				}
+				return response.CreateAccountResponse{
+					Message: "account created",
+				}, nil
+			}, option.Summary("createAccountDispatcher"),
+			option.Header("organization-key", "api organization key", param.Required()),
+			option.Header("country", "api country", param.Required()),
+			option.Tags(tagAccounts),
+		)*/
 }
--- app/adapter/in/fuegoapi/create_account_seller.go
+++ app/adapter/in/fuegoapi/create_account_seller.go
@@ -4,20 +4,19 @@ import (
 	"transport-app/app/shared/infrastructure/httpserver"
 
 	ioc "github.com/Ignaciojeria/einar-ioc/v2"
-	"github.com/go-fuego/fuego"
-	"github.com/go-fuego/fuego/option"
 )
 
 func init() {
 	ioc.Registry(createAccountSeller, httpserver.New)
 }
 func createAccountSeller(s httpserver.Server) {
-	fuego.Post(s.Manager, "/account/seller",
-		func(c fuego.ContextNoBody) (any, error) {
+	/*
+		fuego.Post(s.Manager, "/account/seller",
+			func(c fuego.ContextNoBody) (any, error) {
 
-			return "unimplemented", nil
-		},
-		option.Summary("createAccountSeller"),
-		option.Tags(tagAccounts),
-	)
+				return "unimplemented", nil
+			},
+			option.Summary("createAccountSeller"),
+			option.Tags(tagAccounts),
+		)*/
 }
