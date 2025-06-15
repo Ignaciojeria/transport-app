f8019275a4c7bf9ad57a5b9e009b1a3fafe77b83
ignaciojeria <ignaciovl.j@gmail.com>
2025-06-15 17:56:55 -0400

    use traceID instead uuid for optimizationTracking

--- app/adapter/in/fuegoapi/optimization.go
+++ app/adapter/in/fuegoapi/optimization.go
@@ -1,25 +1,68 @@
 package fuegoapi
 
 import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
 	"transport-app/app/adapter/in/fuegoapi/request"
 	"transport-app/app/adapter/in/fuegoapi/response"
+	"transport-app/app/adapter/out/gcppublisher"
+	"transport-app/app/domain"
 	"transport-app/app/shared/infrastructure/httpserver"
+	"transport-app/app/shared/infrastructure/observability"
+	"transport-app/app/shared/sharedcontext"
 
 	ioc "github.com/Ignaciojeria/einar-ioc/v2"
 	"github.com/go-fuego/fuego"
 	"github.com/go-fuego/fuego/option"
-	"github.com/google/uuid"
 )
 
 func init() {
-	ioc.Registry(optimization, httpserver.New)
+	ioc.Registry(
+		optimization,
+		httpserver.New,
+		gcppublisher.NewApplicationEvents,
+		observability.NewObservability)
 }
-func optimization(s httpserver.Server) {
+
+func optimization(
+	s httpserver.Server,
+	publish gcppublisher.ApplicationEvents,
+	obs observability.Observability) {
 	fuego.Post(s.Manager, "/optimize",
 		func(c fuego.ContextWithBody[request.OptimizationRequest]) (response.OptimizationResponse, error) {
+			spanCtx, span := obs.Tracer.Start(c.Context(), "optimization")
+			defer span.End()
+
+			requestBody, err := c.Body()
+			if err != nil {
+				return response.OptimizationResponse{}, err
+			}
+
+			eventPayload, _ := json.Marshal(requestBody)
+
+			eventCtx := sharedcontext.AddEventContextToBaggage(spanCtx,
+				sharedcontext.EventContext{
+					EntityType: "optimization",
+					EventType:  "optimizationRequested",
+				})
+
+			if err := publish(eventCtx, domain.Outbox{
+				Payload: eventPayload,
+			}); err != nil {
+				return response.OptimizationResponse{}, fuego.HTTPError{
+					Title:  "error requesting optimization",
+					Detail: err.Error(),
+					Status: http.StatusInternalServerError,
+				}
+			}
+
+			obs.Logger.InfoContext(spanCtx,
+				"OPTIMIZATION_REQUESTED_SUCCEEDED",
+				slog.Any("payload", requestBody))
 
 			return response.OptimizationResponse{
-				OptimizationID: uuid.New().String(),
+				TraceID: span.SpanContext().TraceID().String(),
 			}, nil
 		}, option.Summary("optimization"), option.Tags("optimization"))
 }
