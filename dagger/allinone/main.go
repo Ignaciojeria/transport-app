f5fe0d4500336e2316e812b9f940c11636ee3cdd
ignaciojeria <ignaciovl.j@gmail.com>
2025-07-14 23:37:23 -0400

    cicd image exporting improvements

--- dagger/allinone/main.go
+++ dagger/allinone/main.go
@@ -173,5 +173,66 @@ func main() {
 	}
 
 	println("vroom-express clonado a ./vroom-express/")
+
+	// Construir imagen final usando Dagger (equivalente al Dockerfile)
+	println("Construyendo imagen final con Dagger...")
+	finalImage := client.Container().
+		From("debian:bookworm-slim").
+		WithExec([]string{"apt-get", "update"}).
+		WithExec([]string{"apt-get", "install", "-y", "--no-install-recommends",
+			"supervisor",
+			"curl",
+			"git-core",
+			"build-essential",
+			"g++",
+			"libssl-dev",
+			"libasio-dev",
+			"libglpk-dev",
+			"pkg-config",
+			"libssl3",
+			"libglpk40",
+			"nodejs",
+			"npm",
+		}).
+		WithExec([]string{"mkdir", "-p", "/var/log/supervisor", "/var/run", "/conf/optimizer", "/conf/planner", "/vroom-optimizer-app", "/vroom-planner-app", "/transport-app"}).
+		WithDirectory("/data", osrmFiles).
+		WithDirectory("/usr/local/bin", staticBinaries).
+		WithDirectory("/vroom-optimizer", vroomOptimizerDir).
+		WithDirectory("/vroom-planner", vroomPlannerDir).
+		WithDirectory("/vroom-express", vroomExpressRepo).
+		WithDirectory("/transport-app", koAppDir).
+		WithFile("/conf/optimizer/config.yml", client.Host().File("vroom-optimizer/config.yml")).
+		WithFile("/conf/planner/config.yml", client.Host().File("vroom-planner/config.yml")).
+		WithFile("/etc/supervisor/conf.d/osrm.conf", client.Host().File("supervisord.conf")).
+		WithExec([]string{"chmod", "+x", "/usr/local/bin/osrm-routed-static"}).
+		WithExec([]string{"chmod", "+x", "/usr/local/bin/osrm-extract-static"}).
+		WithExec([]string{"chmod", "+x", "/usr/local/bin/osrm-partition-static"}).
+		WithExec([]string{"chmod", "+x", "/usr/local/bin/osrm-customize-static"}).
+		WithExec([]string{"chmod", "+x", "/vroom-optimizer/vroom-optimizer-static"}).
+		WithExec([]string{"chmod", "+x", "/vroom-planner/vroom-planner-static"}).
+		WithExec([]string{"chmod", "+x", "/transport-app/transport-app"}).
+		WithExec([]string{"cp", "-r", "/vroom-express/.", "/vroom-optimizer-app/"}).
+		WithExec([]string{"cp", "-r", "/vroom-express/.", "/vroom-planner-app/"}).
+		WithExec([]string{"ln", "-sf", "/vroom-optimizer/vroom-optimizer-static", "/vroom-optimizer-app/vroom"}).
+		WithExec([]string{"ln", "-sf", "/vroom-planner/vroom-planner-static", "/vroom-planner-app/vroom"}).
+		WithExec([]string{"cp", "/conf/optimizer/config.yml", "/vroom-optimizer-app/config.yml"}).
+		WithExec([]string{"cp", "/conf/planner/config.yml", "/vroom-planner-app/config.yml"}).
+		WithWorkdir("/vroom-optimizer-app").
+		WithExec([]string{"npm", "install"}).
+		WithWorkdir("/vroom-planner-app").
+		WithExec([]string{"npm", "install"}).
+		WithExposedPort(5000).
+		WithExposedPort(3000).
+		WithExposedPort(3001).
+		WithExposedPort(9001).
+		WithEntrypoint([]string{"supervisord", "-c", "/etc/supervisor/conf.d/osrm.conf"})
+
+	// Exportar la imagen final
+	_, err = finalImage.Export(ctx, "transport-app-allinone.tar")
+	if err != nil {
+		panic(err)
+	}
+
+	println("Imagen final exportada como transport-app-allinone.tar")
 	println("Contenedor final sincronizado")
 }
