766d219aeb434c9e6a5223e38e378e4c35a491ef
ignaciojeria <ignaciovl.j@gmail.com>
2025-08-13 13:54:13 -0400

    vertex ai message improvements done

--- app/shared/infrastructure/ai/client.go
+++ app/shared/infrastructure/ai/client.go
@@ -2,6 +2,7 @@ package ai
 
 import (
 	"context"
+	"fmt"
 	"transport-app/app/shared/configuration"
 
 	ioc "github.com/Ignaciojeria/einar-ioc/v2"
@@ -13,6 +14,14 @@ func init() {
 }
 
 func NewClient(conf configuration.Conf) (*genai.Client, error) {
+	if conf.GOOGLE_PROJECT_ID == "" {
+		fmt.Println("Vertex AI will not be initialized because GOOGLE_PROJECT_ID is not set")
+		return nil, nil
+	}
+	if conf.GOOGLE_PROJECT_LOCATION == "" {
+		fmt.Println("Vertex AI will not be initialized because GOOGLE_PROJECT_LOCATION is not set")
+		return nil, nil
+	}
 	return genai.NewClient(context.Background(), &genai.ClientConfig{
 		Project:  conf.GOOGLE_PROJECT_ID,
 		Location: conf.GOOGLE_PROJECT_LOCATION,
