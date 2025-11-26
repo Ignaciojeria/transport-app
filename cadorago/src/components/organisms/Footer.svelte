8c87eb0e47257d28e610080726707b6b17b469a9
ignaciojeria <ignaciovl.j@gmail.com>
2025-11-26 18:34:38 -0300

    footer improved

--- cadorago/src/components/organisms/Footer.svelte
+++ cadorago/src/components/organisms/Footer.svelte
@@ -1,5 +1,5 @@
 <script>
-  import BrandLogo from '../molecules/BrandLogo.svelte';
+  import Logo from '../atoms/Logo.svelte';
   
   const { className = '' } = $props();
 </script>
@@ -7,7 +7,7 @@
 <footer class={`bg-[#E8E4D9] py-8 sm:py-12 lg:py-16 ${className}`}>
   <div class="container mx-auto px-4 sm:px-6 lg:px-12 max-w-[1600px]">
     <div class="flex justify-center">
-      <BrandLogo size="footer" />
+      <Logo className="h-24 sm:h-32 lg:h-40 w-auto" alt="Cadorago Logo" />
     </div>
   </div>
 </footer>
