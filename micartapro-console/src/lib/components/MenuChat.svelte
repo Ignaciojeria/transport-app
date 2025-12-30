606d259a55fbb9ee478ce6753bd81600a0ddb664
ignaciojeria <ignaciovl.j@gmail.com>
2025-12-30 18:22:50 -0300

    UPGRADE TO PRO changed to Unlock Pro

--- micartapro-console/src/lib/components/MenuChat.svelte
+++ micartapro-console/src/lib/components/MenuChat.svelte
@@ -365,7 +365,7 @@
           <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7l5 5m0 0l-5 5m5-5H6" />
           </svg>
-          UPGRADE TO PRO
+          Unlock Pro
         </span>
       </button>
     </div>
